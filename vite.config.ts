1db69f047cfa9359be0d40dd4a2cb9779727d7f6
Allyson Amboni <allysonamboni@gmail.com>
2023-11-30 18:58:31 +0800

    Added PWA config and logos

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'logo.svg'],
+      manifest: {
+        name: 'Meal Mix',
+        short_name: 'MealMix',
+        description: 'Generate your meals',
+        theme_color: '#D35933',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

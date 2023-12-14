fea6dc5e195e88a06d160f896768334eb5507b5e
Allyson Amboni <allysonamboni@gmail.com>
2023-12-15 00:49:07 +0800

    Removed text-sm default size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@ import Navbar from './partials/Navbar.vue'
 </script>
 
 <template>
-  <div class="h-screen flex flex-col justify-between bg-zinc text-sm">
+  <div class="h-screen flex flex-col justify-between bg-zinc">
     <div class="flex-1 flex flex-col gap-2 p-6 overflow-auto">
       <slot />
     </div>

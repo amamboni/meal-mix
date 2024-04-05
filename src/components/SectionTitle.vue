5145b7b0820ba5dfe74fa214bf60104f2f1116cc
Allyson Amboni <allysonamboni@gmail.com>
2024-04-05 23:56:13 +0800

    Update SectionTitle.vue

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col">
-    <h1 class="text-lg font-semibold"><slot name="title" /></h1>
+    <h1 class="text-lg font-semibold">
+      <slot name="title" />
+    </h1>
     <p v-if="$slots.description" class="text-gray"><slot name="description" /></p>
   </div>
 </template>
